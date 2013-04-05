82f1399794d4f0d348601cc76a5d61564d3ff861
Oriol <oriol@oriol-K52Je.(none)>
2013-04-05 19:27:57 +0200

    Add relations between classes (Player with Representative)(League with Teams)(Team with Players)

--- sitw/prac1/models.py
+++ sitw/prac1/models.py
@@ -13,6 +13,7 @@ class Player(models.Model):
 	bornDate = models.DateField()
 	nacionality = models.CharField(max_length=50)
 	position = models.CharField(max_length=20)
+	representative = models.ForeignKey(Representative)
 	def __unicode__(self):
 		return self.name+ " - "+self.position
 
