216d63d4f6b8f07989b1d486207a3503ea59657f
Oriol <oriol@oriol-K52Je.(none)>
2013-04-05 13:08:18 +0200

    List the leagues in the mainpage

--- sitw/prac1/views.py
+++ sitw/prac1/views.py
@@ -2,29 +2,18 @@ from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
-
-def userpage(request, username):
-	try:
-		user = User.objects.get(username=username)
-	except:
-		raise Http404('User not found.')
-
-	sobres = user.sobre_set.all()
-	template = get_template('userpage.html')
-	variables = Context({
-		'username': username,
-		'sobres': sobres
-	})
-	output = template.render(variables)
-	return HttpResponse(output)
+import sqlite3
+from models import League
 
 def mainpage(request):
+	leagues = League.objects.all()
 	template = get_template('mainpage.html')
 	variables = Context({
 		'titlehead': 'Football League App',
 		'pagetitle': 'Welcome to the Football League App',
 		'contentbody': 'Managing non legal funding since 2013',
 		'user': request.user,
+		'leagues': leagues,
 		})
 	output = template.render(variables)
 	return HttpResponse(output)
