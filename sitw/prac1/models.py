85b2911696419908f32f3290a672d6da14123de8
Oriol <oriol@oriol-K52Je.(none)>
2013-04-05 18:50:23 +0200

    List all the items of League, Team and Player

--- sitw/prac1/models.py
+++ sitw/prac1/models.py
@@ -4,19 +4,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class League(models.Model):
-	creationDate = models.DateTimeField()
-	name = models.CharField(max_length=50)
-	description = models.TextField(max_length=100)
-	def __unicode__(self):
-		return self.name+" - "+self.description
-
-class Team(models.Model):
-	teamName = models.CharField(max_length=50)
-	foundationYear = models.IntegerField()
-	def __unicode__(self):
-		return self.teamName
-
 class Player(models.Model):
 	playerName = models.CharField(max_length=50)
 	bornDate = models.DateField()
@@ -24,3 +11,18 @@ class Player(models.Model):
 	position = models.CharField(max_length=20)
 	def __unicode__(self):
 		return self.playerName+ " - "+self.position
+
+class Team(models.Model):
+	teamName = models.CharField(max_length=50)
+	foundationYear = models.IntegerField()
+	players = models.ManyToManyField(Player)
+	def __unicode__(self):
+		return self.teamName
+
+class League(models.Model):
+	creationDate = models.DateTimeField()
+	name = models.CharField(max_length=50)
+	description = models.TextField(max_length=100)
+	teams = models.ManyToManyField(Team)
+	def __unicode__(self):
+		return self.name+" - "+self.description
--- sitw/prac1/views.py
+++ sitw/prac1/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
-import sqlite3
-from models import League
+from models import League,Team,Player
 
 def mainpage(request):
 	leagues = League.objects.all()
+	teams = Team.objects.all()
+	players = Player.objects.all()
 	template = get_template('mainpage.html')
 	variables = Context({
 		'titlehead': 'Football League App',
@@ -14,6 +15,8 @@ def mainpage(request):
 		'contentbody': 'Managing non legal funding since 2013',
 		'user': request.user,
 		'leagues': leagues,
+		'teams': teams,
+		'players': players,
 		})
 	output = template.render(variables)
 	return HttpResponse(output)
