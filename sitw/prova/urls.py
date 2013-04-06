2165889d2c0589627ab8d6e2736a071d33c07247
Oriol <oriol@oriol-K52Je.(none)>
2013-04-06 18:06:45 +0200

    Add the urls for list leagues, teams and players

--- sitw/prova/urls.py
+++ sitw/prova/urls.py
@@ -8,8 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
 	url(r'^$', mainpage, name='home'),
-	url(r'^user/(\w+)/$', userpage),
-	url(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^teamslist/$', teamslist, name='List of Teams'),
+	url(r'^playerslist/$', playerslist, name='List of Players'),
+	url(r'^leagueslist/$', leagueslist, name='List of Leagues'),
+	#url(r'^user/(\w+)/$', userpage),
+	#url(r'^login/$', 'django.contrib.auth.views.login'),
     # url(r'^$', 'prova.views.home', name='home'),
     # url(r'^prova/', include('prova.foo.urls')),
 
