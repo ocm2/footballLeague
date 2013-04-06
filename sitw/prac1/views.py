c82bf80a40ecd30dc5705be33e52f4eca340ebec
Oriol <oriol@oriol-K52Je.(none)>
2013-04-06 18:03:22 +0200

    Add pages for list items

--- sitw/prac1/views.py
+++ sitw/prac1/views.py
@@ -4,19 +4,43 @@ from django.template.loader import get_template
 from django.contrib.auth.models import User
 from models import League,Team,Player
 
-def mainpage(request):
-	leagues = League.objects.all()
-	teams = Team.objects.all()
-	players = Player.objects.all()
+def mainpage(request):	
 	template = get_template('mainpage.html')
 	variables = Context({
 		'titlehead': 'Football League App',
 		'pagetitle': 'Welcome to the Football League App',
 		'contentbody': 'Managing non legal funding since 2013',
 		'user': request.user,
-		'leagues': leagues,
-		'teams': teams,
-		'players': players,
 		})
 	output = template.render(variables)
 	return HttpResponse(output)
+
+def leagueslist(request):
+	leagues = League.objects.all()
+	template = get_template('listPages/list.html')
+	variables = Context({
+		'title': 'List of Leagues',
+		'items': leagues,
+	})
+	output = template.render(variables)
+	return HttpResponse(output)	
+
+def teamslist(request):
+	teams = Team.objects.all()
+	template = get_template('listPages/list.html')
+	variables = Context({
+		'title': 'List of Teams',
+		'items': teams,
+	})
+	output = template.render(variables)
+	return HttpResponse(output)	
+
+def playerslist(request):
+	players = Player.objects.all()
+	template = get_template('listPages/list.html')
+	variables = Context({
+		'title': 'List of Players',
+		'items': players,
+	})
+	output = template.render(variables)
+	return HttpResponse(output)	
